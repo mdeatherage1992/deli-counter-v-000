8acf3dfcc1bac147f7858c744f79bc9fcf6a84c2
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 14:15:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length > 0
   katz_deli.each_with_index do |name,index|
-  end
     puts "The line is currently:#{index + 1}. #{name}"
+  end
 end
 end
 
