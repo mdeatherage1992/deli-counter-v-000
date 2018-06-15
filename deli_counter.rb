2f9cf4bb315916bb5616a3d809f6b0eb331fb252
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 14:28:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
     katz_deli.each_with_index do |name,index|
       list = []
       list.push("#{index}.#{name}")
-    puts "The line is currently: #{index + 1}. #{name}"
   end
+  puts "puts The line is currently: " + list.join(' ')
 end
 end
 
