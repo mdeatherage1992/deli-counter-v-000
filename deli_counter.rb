7f53a9cdd220f5bdd3ae0d9ac10a8a7f5e31ce7c
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 14:14:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0
+  katz_deli.each_with_index do |name,index|
+  end
+    puts "The line is currently:#{index + 1}. #{name}"
+end
+end
+
+def take_a_number(array,person)
+  array.push(person)
+  puts "Welcome, #{person}. You are number #{array.length} in line."
+end
